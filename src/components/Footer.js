db2771eb5f2c058e82cbb43c81abf184704cf947
Jonah Bitautas <jbitautas@gmail.com>
2022-07-08 23:37:17 -0500

    Initial commit.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <p>Quote generator by @jonahvsweb &copy; 2022</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Quote.js
@@ -0,0 +1,10 @@
+const Quote = () => {
+  return (
+    <div>
+      <blockquote>Temporary quote</blockquote>
+      <p>- The name</p>
+    </div>
+  );
+};
+
+export default Quote;
