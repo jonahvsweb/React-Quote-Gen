b87bab92570caa9dc3819cbe5f53bf7184b15da1
Jonah Bitautas <jbitautas@gmail.com>
2022-07-08 23:41:26 -0500

    Import components to app.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import "./App.css";
+import Quote from "./components/Quote";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <section>
       <div className="App">
         <h1>Quote Generator</h1>
-        {}
+        <Quote />
         <button>Generate quote</button>
       </div>
-      {}
+      <Footer />
     </section>
   );
 }
